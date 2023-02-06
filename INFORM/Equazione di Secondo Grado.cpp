d71fc9f3ff4d80dd465ee7f8090cfd1a949a11ea
Gytex8Hub <123359240+Gytex8Hub@users.noreply.github.com>
2023-02-06 10:51:34 +0100

    Create Equazione di Secondo Grado.cpp

---
+++ INFORM/Equazione di Secondo Grado.cpp
@@ -0,0 +1,46 @@
+/*
+    Name:Santangelo_Es.FunzioneMedia
+    Copyright:Colamonico-Chiarulli
+    Author:Gianluigi Santangelo
+    Date: 28/09/22 09:39
+    Description:
+*/
+
+
+//1. includo le librerie
+#include <iostream>
+#include <cmath>
+using namespace std;
+//2. inizio il blocco main
+
+double CalcolaDelta 
+	
+int main(){
+//3. dichiaro e inizializzo le variabili
+double b;
+double esp;
+
+cout<<"inserisci la base ";
+cin>>b;
+cout<<"inserisci l'esponente ";
+cin>>esp;
+//4. input
+
+
+//5. logica - operazioni - algoritmo
+
+
+//6. output
+
+
+//7. ritorno al sistema operativo
+return 0;
+//8. fine del programma
+}
+double CalcolaDelta (double a,double b,double c){
+	double delta;
+	delta = pow(b,esp)-4*a*c;
+	double delta = sqrt(pow(b,esp)-4*a*c);
+	return delta;
+
+}

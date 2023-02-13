c44cc304be8779333b792c456183536b2d63a200
Gytex8Hub <123359240+Gytex8Hub@users.noreply.github.com>
2023-02-13 10:58:44 +0100

    .

---
+++ INFORM/Santangelo_Es.QuantiBisestili_2°.cpp
@@ -0,0 +1,51 @@
+/*
+    Name:Santangelo_Es.QuantiBisestili_2°
+    Copyright:Colamonico-Chiarulli
+    Author:Gianluigi Santangelo
+    Date: 28/09/22 09:39
+    Description:
+*/
+
+
+//1. includo le librerie
+#include <iostream>
+#include <cmath>
+using namespace std;
+//2. inizio il blocco main
+
+double N_A_Bisestili(int anno1, int anno2);
+	
+int main(){
+//3. dichiaro e inizializzo le variabili
+int anno1;
+int anno2;
+int annib;
+//4. input
+
+cout<<"inserisci il primo anno ";
+cin>>anno1;
+cout<<"inserisci il secondo anno ";
+cin>>anno2;
+cout
+//5. logica - operazioni - algoritmo
+
+
+//6. output
+
+
+//7. ritorno al sistema operativo
+return 0;
+//8. fine del programma
+}
+double N_A_Bisestili(int anno1, int anno2){
+	int annib;
+while(anno1<=anno2) {
+	if(anno1%100==0 anno2%400==0) {
+		annib=annib+1;
+		
+}
+}
+	{
+	}
+	
+}

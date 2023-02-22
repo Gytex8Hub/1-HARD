811ce4d7882d3616c9a49c3dea837317bb5e041b
Gytex8Hub <123359240+Gytex8Hub@users.noreply.github.com>
2023-02-22 13:20:29 +0100

    +python

---
+++ INFORM/triangolo.py
@@ -0,0 +1,10 @@
+import math
+base = float(input("inserisci la base del triangolo: "))
+altezza =float(input("inserisci la base del triangolo: "))
+primolato =float(input("inserisci il 1° lato del triangolo: "))
+secondolato =float(input("inserisci il 2° lato del triangolo: "))
+terzolato =float(input("inserisci il 3° lato del triangolo: "))
+area = (base*altezza)/2
+perimetro = primolato+secondolato+terzolato
+print(area)
+print(perimetro)

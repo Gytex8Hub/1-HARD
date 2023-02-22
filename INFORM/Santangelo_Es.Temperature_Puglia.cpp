5ab1b1c8c01df9256c96580b19b670c0e46b191a
Gytex8Hub <123359240+Gytex8Hub@users.noreply.github.com>
2023-02-22 13:49:59 +0100

    .

--- INFORM/Santangelo_Es.Temperature_Puglia.cpp
+++ INFORM/Santangelo_Es.Temperature_Puglia.cpp
@@ -1,42 +1,60 @@
 /*
-    Name:Santangelo_Es.Temperature_Puglia
-    Copyright:Colamonico-Chiarulli
-    Author:Gianluigi Santangelo
-    Date: 28/09/22 09:39
-    Description:
+	Name: Gianluigi_Temperatura
+	Copyright: Colamonico Chiarulli
+	Author: Antonio Solare
+	Date: 15/02/23 13:47
+	Description: Dati in input due vettori di sei numeri interi contenenti le temperature minime registrate in due giorni diversi nei capoluoghi di provincia pugliesi, calcolare la media di ciascun giorno, dare in output la media stessa e dire quale vettore ha la media pi? alta.
 */
 
-
-//1. includo le librerie
 #include <iostream>
-#include <cmath>
 using namespace std;
-//2. inizio il blocco main
 
+double Temperatura(double temp[])
+{
+	int i;
+	double somma,media=0;
 	
-int main(){
-//3. dichiaro e inizializzo le variabili
-
-int i;
-int lunghVet=0;
-int vettore1[i];
-int vettore2[i];
-
-cout<<"inserisci la lunghezza del vettore";
-cin>>lunghVet;
-
-for(i=0;i<lunghVet;i++) {
-	cout<<"inserisci il valore della temperatura del vettore 1,"<<i<<" : ";
-	cin>>vettore1[i];
+	for(i = 1; i < 7 ; i++)
+	{
+		somma += temp[i];
 	}
 	
-for(i=0;i<lunghVet;i++) {
-	cout<<"inserisci il valore della temperatura del vettore 2,"<<i<<": ";
-	cin>>vettore2[i];
+	media = somma/6;
+	
+	return media;
+}
+
+int main()
+{
+	const int MAX=100;
+	double temp[MAX];
+	int i;
+	double media1 , media2;
+	
+	for(i = 1; i < 7 ; i++)
+	{
+		cout << "Inserisci la temperatura numero " << i << " del primo giorno: ";
+		cin >> temp[i];
 	}
 	
-//7. ritorno al sistema operativo
-return 0;
-//8. fine del programma
+	media1 = Temperatura(temp);
 	
+	
+	for(i = 1; i < 7 ; i++)
+	{
+		cout << "Inserisci la temperatura numero " << i << " del secondo giorno: ";
+		cin >> temp[i];
+	}
+	
+	media2 = Temperatura(temp);
+	
+	cout << "La temperatura media del primo giorno e\': " << media1;
+	cout << endl;
+	cout << "La temperatura media del secondo giorno e\': " << media2;
+	cout << endl;
+	
+	if(media1>media2)
+		cout << "La temperatura media e\' piu\' alta durante il primo giorno";
+	else
+		cout << "La temperatura media e\' piu\' alta durante il secondo giorno";
 }

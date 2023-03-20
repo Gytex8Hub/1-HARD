ffd2f1c64d7366d9848143a4f50ab5514e88ff7c
Gytex8Hub <123359240+Gytex8Hub@users.noreply.github.com>
2023-03-20 10:45:28 +0100

    Update Santangelo_ES.Disegnare_una_figura_Geometrica.py

--- INFORM/Santangelo_ES.Disegnare_una_figura_Geometrica.py
+++ INFORM/Santangelo_ES.Disegnare_una_figura_Geometrica.py
@@ -1,13 +1,13 @@
 import turtle
 v=turtle.Pen()
 nlati=int(input('inserisci il numero di lati'))
+llati=int(input('inserisci la lunghezza del lato'))
 
 tot_ang=(nlati-2)*180
-print(tot_ang)
 ang_est=180-(tot_ang//nlati)
 
 for i in range(nlati):
-    v.forward(100)
+    v.forward(llati)
     v.left(ang_est)
     
     
